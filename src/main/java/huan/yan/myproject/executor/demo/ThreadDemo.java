0644d32a0471085e92290633c5f1838e2cb0a232
yanhuan529369769 <529369769@qq.com>
2019-04-29 17:37:29 +0800

    callable

--- src/main/java/huan/yan/myproject/executor/demo/ThreadDemo.java
+++ src/main/java/huan/yan/myproject/executor/demo/ThreadDemo.java
@@ -1,11 +1,7 @@
 package huan.yan.myproject.executor.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.Date;
+import java.util.concurrent.*;
 
 public class ThreadDemo {
 
@@ -13,27 +9,33 @@ public class ThreadDemo {
     private static final ExecutorService executor = Executors.newFixedThreadPool(5);
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        long l = System.currentTimeMillis();
+        /*long l = System.currentTimeMillis();
         List<Future> list = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             int finalI = i;
-            Future<?> submit = executor.submit(new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println(Thread.currentThread().getName() + ":" + finalI);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+            Future<?> submit = executor.submit(() -> {
+                System.out.println(Thread.currentThread().getName() + ":" + finalI);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             });
-//            System.out.println("submit:" + submit.get());
+            System.out.println("submit:" + submit.isDone());
             list.add(submit);
         }
+        Thread.sleep(10000);
         long l2 = System.currentTimeMillis();
         System.out.println("cost time:" + (l2 - l));
-        System.out.println("list:" + list);
+        for (Future future : list) {
+            System.out.println(future.isDone());
+        }*/
+        for (int i = 0; i < 20; i++) {
+            int finalI = i;
+            Future<?> submit = executor.submit((Callable) () -> Thread.currentThread().getName()+ "aa" + finalI);
+            System.out.println(submit.get());
+        }
+
     }
 
 }
